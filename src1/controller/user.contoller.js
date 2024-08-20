4a317243f0fc94381079866585fa1b2c7e6be858
yogesh-013 <yogeshpaneru56@gmail.com>
2024-08-20 18:08:10 +0530

    implemented aggregated pipelines between  subscription model and user model

--- src1/controller/user.contoller.js
+++ src1/controller/user.contoller.js
@@ -314,7 +314,72 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
         new ApiResponse(200, user, "Cover image updated successfully")
     )
 })
+//////////////////////////////////////
+const getUserChannnelProfile = asyncHandler(async (req , res)=>{
+  const username = req.params 
+  if(!username){
+    throw new ApiError(400 , "Username not found")
+  }
+    const channel = await User.aggregate([
+      {
+        $match : {
+          username : username?.toLowerCase() 
+        }
+      },
+      {
+        $lookup : {
+          from : "subscriptions" , 
+          localField : "_id" , 
+          foreignField : "channel" , 
+          as : "subscribers"
+        }
+      }, 
+      {
+        $lookup : {
+          from : "subscriptions" , 
+          localField : "_id" , 
+          foreignField : "subscriber" , 
+          as : "subscribedTo"
+        }
 
+      },
+      {
+        $addFields : {
+          subscriberCount : {
+            $size : "$subscribers"
+          },
+          channelSubscribesToCount : {
+            $size : "$subscribedTo"
+          }, 
+          isSubscribed : {
+            $cond : {
+              if :{$in : [req.user?._id , "$subscribers.subscriber"]},
+              then : true , 
+              else : false 
+
+            }
+          }
+        }
+      }, 
+      {
+        $project : {
+          fullname : 1 , 
+          username : 1 , 
+          subscriberCount : 1 ,
+          channelSubscribesToCount : 1, 
+          isSubscribed : 1  , 
+          avatar : 1 , 
+          coverImage : 1 , 
+          email : 1 
+        }
+      }
+    ])
+    if(!channel){
+      throw new ApiError(400 , "Username does not exist")
+    }
+    res.status(200)
+    .json(new ApiResponse(200 , channel[0] , "User Details Fetched Successfully"))
+})
 
 export {registerUser ,
   loginUser , 
@@ -323,6 +388,7 @@ export {registerUser ,
 changeCurrentPassword , 
 getCurrentUser , 
 updateUserAvatar , 
-updateUserCoverImage
+updateUserCoverImage , 
+getUserChannnelProfile
 
 };
